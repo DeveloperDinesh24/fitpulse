614a870e88d18e25498e8f2cb02de0319f501fa4
Dinesh <1442dinesh6singh5@gmail.com>
2026-04-17 13:03:49 +0530

    feat: add responsive glassmorphism navbar

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,9 @@
+import { Navbar } from '../components/sections/Navbar'
+
+export default function LandingPage() {
+  return (
+    <div className='min-h-screen bg-dark text-white selection:bg-brand selection:text-black font-sans'>
+      <Navbar />
+    </div>
+  )
+}
