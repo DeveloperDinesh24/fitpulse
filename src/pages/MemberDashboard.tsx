e644cea3b82ad09daef4079f038fbdb51a5f3508
Dinesh <1442dinesh6singh5@gmail.com>
2026-04-20 13:00:16 +0530

    feat: add about(hardcoded)

--- src/pages/MemberDashboard.tsx
+++ src/pages/MemberDashboard.tsx
@@ -1,3 +1,4 @@
+import { About } from '../components/dashboard/About'
 import { Header } from '../components/dashboard/Header'
 
 export const MemberDashboard = () => {
@@ -5,6 +6,7 @@ export const MemberDashboard = () => {
     <div className='min-h-screen bg-dark text-white p-6 md:p-8 flex gap-8'>
       <main className='flex-1 space-y-8'>
         <Header />
+        <About />
       </main>
     </div>
   )
