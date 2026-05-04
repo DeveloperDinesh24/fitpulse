21cce2fbc092ed122547663cd1f6f54ee73efc4f
Dinesh <1442dinesh6singh5@gmail.com>
2026-05-04 08:51:39 +0530

    feat: add stats bar in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { HeroSection } from '@/components/sections/HeroSection'
-import { Navbar } from '../components/sections/Navbar'
+import { Navbar } from '@/components/sections/Navbar'
+import { StatsBar } from '@/components/sections/StatsBar'
 
 export default function Home() {
   return (
     <div className='min-h-screen bg-dark text-white selection:bg-brand selection:text-black font-sans'>
       <Navbar />
       <HeroSection />
+      <StatsBar />
     </div>
   )
 }
