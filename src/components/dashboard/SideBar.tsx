2be6e3139180e719bdff4033c153c7c965cb9d2f
Dinesh <1442dinesh6singh5@gmail.com>
2026-05-05 11:16:07 +0530

    feat(dashboard): add system info card in sidebar

--- src/components/dashboard/SideBar.tsx
+++ src/components/dashboard/SideBar.tsx
@@ -1,9 +1,11 @@
 import { PlanCard } from './PlanCard'
+import { SystemInfoCard } from './SystemInfoCard'
 
 export const SideBar = () => {
   return (
     <div className='space-y-6 flex flex-col h-full'>
       <PlanCard />
+      <SystemInfoCard />
     </div>
   )
 }
